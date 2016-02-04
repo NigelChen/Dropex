3b55037ed8ef0ae5dd69225728399b65fbff829f
nigelchen <nigelchen2014@gmail.com>
2016-02-03 20:53:18 -0800

    added recaptcha

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,71 +26,78 @@ class PagesController < ApplicationController
   end
   
   def uploadFile
-    fileObject = params[:upload][:file] #the file
-    timeToDestroy = params[:upload][:time] #timer in (units)
-    units = params[:upload][:units] #units of time. {'min','hours'}
-    maxlim = params[:upload][:interval] #download limit
-    code = params[:upload][:code] #custom code
-    
-    #checks the size of the file if the client side fails to.
-    if fileObject.size >=52428801
-      flash[:notice] = "File size is too large. Needs to be under 51MB."
+    if not verify_recaptcha
+      flash[:notice] = "You need to complete the recaptcha."
       redirect_to "/"
-    end
-    
-    #check the length of the code. handles accordingly
-    if code.length <= 0
-      code = SecureRandom.hex(3)
-      while not Defile.find_by_filecode(code).nil?
+    else
+
+      fileObject = params[:upload][:file] #the file
+      timeToDestroy = params[:upload][:time] #timer in (units)
+      units = params[:upload][:units] #units of time. {'min','hours'}
+      maxlim = params[:upload][:interval] #download limit
+      code = params[:upload][:code] #custom code
+      
+      #checks the size of the file if the client side fails to.
+
+      if fileObject.size >=52428801
+        flash[:notice] = "File size is too large. Needs to be under 51MB."
+        redirect_to "/"
+      end
+      
+      #check the length of the code. handles accordingly
+      if code.length <= 0
+        code = SecureRandom.hex(3)
+        while not Defile.find_by_filecode(code).nil?
+          maskedFileName = SecureRandom.hex
+        end
+      end
+      
+      #checks the length of the download limit. handles accordingly
+      if maxlim.length <=0
+        if cookies[:interval]
+          maxlim = cookies[:interval].to_i
+        else
+          maxlim = 1
+        end
+      end
+      
+      #checks the timer. handles accordingly
+      if timeToDestroy.length <= 0
+        if cookies[:time]
+          timeToDestroy = cookies[:time].to_i
+        else 
+          timeToDestroy = 10
+        end
+      end
+      
+      timeToDestroy = timeToDestroy.to_i
+      
+      #checks the units of the cookies/website.
+      if units == 'min' or (cookies[:units] == 'min')
+        hours = DateTime.now + timeToDestroy.minutes
+      else
+        hours = DateTime.now + timeToDestroy.hours
+      end
+      
+      
+      #creates the file name.
+      maskedFileName = SecureRandom.hex 
+      while Defile.exists?(maskedFileName)
         maskedFileName = SecureRandom.hex
       end
-    end
-    
-    #checks the length of the download limit. handles accordingly
-    if maxlim.length <=0
-      if cookies[:interval]
-        maxlim = cookies[:interval].to_i
-      else
-        maxlim = 1
+      
+      #creates file object. save into databse
+      Defile.create(name: fileObject.original_filename, maskedName: maskedFileName, toDestroy: hours, filecode: code,maxlim: maxlim,downloads:0)
+      
+      #saves into the disk
+      File.open(Rails.root.join('public','files', maskedFileName), 'wb') do |f|
+        f.write(fileObject.read)
       end
-    end
-    
-    #checks the timer. handles accordingly
-    if timeToDestroy.length <= 0
-      if cookies[:time]
-        timeToDestroy = cookies[:time].to_i
-      else 
-        timeToDestroy = 10
-      end
-    end
-    
-    timeToDestroy = timeToDestroy.to_i
-    
-    #checks the units of the cookies/website.
-    if units == 'min' or (cookies[:units] == 'min')
-      hours = DateTime.now + timeToDestroy.minutes
-    else
-      hours = DateTime.now + timeToDestroy.hours
-    end
-    
-    
-    #creates the file name.
-    maskedFileName = SecureRandom.hex 
-    while Defile.exists?(maskedFileName)
-      maskedFileName = SecureRandom.hex
-    end
-    
-    #creates file object. save into databse
-    Defile.create(name: fileObject.original_filename, maskedName: maskedFileName, toDestroy: hours, filecode: code,maxlim: maxlim,downloads:0)
-    
-    #saves into the disk
-    File.open(Rails.root.join('public','files', maskedFileName), 'wb') do |f|
-      f.write(fileObject.read)
-    end
 
-    flash[:success] = "Your file code is #{code} " 
-    #yay, finished! redirect    
-    redirect_to "/"
+      flash[:success] = "Your file code is #{code} " 
+      #yay, finished! redirect    
+      redirect_to "/"
+    end
   end
   
   
