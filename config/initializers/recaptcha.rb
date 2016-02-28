a5b0a30b78eb8a82c2462798482aab5f5759ec29
Joshua <joshuawu3@gmail.com>
2016-02-28 10:06:53 -0800

    Update recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LcNXBcTAAAAACXlLXk98eQtbiLNAd5p0iaFVhzq'
-  config.private_key = '6LcNXBcTAAAAAPyEuJkbDTMYCJL5NPUQvc-Wl0v4'
+  config.public_key  = '########################################'
+  config.private_key = '########################################'
 end
